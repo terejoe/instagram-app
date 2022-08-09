ce7982718d465c31ffb023346981e31aba002b96
terejoe <nzeamalunkechinyere@gmail.com>
2022-08-09 20:43:24 +0100

    No Photos modified

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -34,7 +34,6 @@ const Gallery = () => {
     
     return (
         <>
-            <div className="null-photo">No Photos here</div>
             <input type ="file" name = "photo" id = "addPhotoInput" />
             <label htmlFor="addPhotoInput" onClick={addPhoto}>
                 <i className="add-photo-button fas fa-plus-square"></i>
@@ -42,14 +41,16 @@ const Gallery = () => {
             <button htmlFor= "deleteAllPhoto" onClick={() => deletePhotos()}>
                 <i className="delete-all fa-solid fa-trash"></i>
             </button>
+            
 
             <section className="gallery">
                 {!allPhotos && <Waveform size={90} lineWeight ={3.5} speed={1} color ="black"/>}
+                {allPhotos.length === 0 ? <div className="null-photo">No Photos here</div>:<></>}
                 {allPhotos?.map((photo) => (
                     <div className="item" key={photo.id}>
                         <img src ={photo.url} className="item-image" alt=''></img>
                         <button className="delete-button" onClick ={() => removePhoto(photo.id)}>Delete</button>
-                    </div>
+                    </div> 
                 ))}
                 
             </section>
